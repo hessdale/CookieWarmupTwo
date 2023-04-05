09b5f01ef2d6defbaad1aba102c26cb184eb07b1
dale <hessdale96@gmail.com>
2023-04-04 19:13:33 -0600

    warming up with cookies

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ monkeys_html.insertAdjacentHTML(`afterend`, `<div id="monkey">
 </div><div id="monkey">
 <img src="/images/monkey_big_ears.jpeg" alt="funny monkey" width="200px">
 </div><div id="monkey">
-<img src="/images/sad_chimp.jpeg" alt="funny monkey" width="200px">
+<img src="/images/sad_chimp.jpg" alt="sad chimp" width="200px">
 </div>`);
 
 function store_cookie(details) {
@@ -14,8 +14,8 @@ function store_cookie(details) {
 }
 
 
-let monkey = document.getElementById(`monkey`);
+let monkey = document.querySelectorAll(`img`);
 
 for (i = 0; i < monkey.length; i++) {
-    monkey.addEventListener(`click`,)
+    monkey[i].addEventListener(`click`, store_cookie);
 }
